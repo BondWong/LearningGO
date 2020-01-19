f75a69b035261eb5253021fd93e5ebf5cd34eba4
Bond Wong <bond@Bonds-MacBook-Pro.local>
2020-01-19 14:37:10 -0800

    chapter 2

---
+++ src/golang-book/chapter2/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("1 + 1 =", 1+1)
+	fmt.Println("1.0 + 1.0 =", 1.0+1.0)
+	fmt.Println("1.0 * 1.0 =", 1.0*1.0)
+	fmt.Println("length of hi is", len("hi"))
+	fmt.Println("hi"[0])
+	fmt.Println(32123 * 42452)
+}
